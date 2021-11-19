e23b4f328a29afefd216c6eb9565424de50b718a
大溝桂 <kay@KeiOmizo-MacBook-Pro.local>
2021-11-19 12:30:39 +0900

    update

--- BlobTrigger1/index.js
+++ BlobTrigger1/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, inBlob) {
     context.log("JavaScript blob trigger function processed blob \n Blob:", context.bindingData.blobTrigger, "\n Blob Size:", inBlob.length, "Bytes");
 
-    context.bidings.outBlob = context.bindings.inBlob;
+    context.bindings.outBlob = context.bindings.inBlob;
     context.done();
 };
